cf83313e732d8b679cde6982d33b76fcf0798e1b
Wilgenfors <wilgenfors@mail.ru>
2025-01-24 13:39:57 +0500

    Update index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация и Вход</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Подключаем CSS файл -->
+</head>
+<body>
+    <main>
+        
+         <form class="form">
+             <h2>Добро пожаловать!</h2>
+        <div class="links">
+            <a href="registration.php">Регистрация</a><br>
+            <a href="authorization.php">Вход</a>
+        </div>
+        </form>
+    </main>
+</body>
+</html>
