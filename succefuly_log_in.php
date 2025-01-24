5345906c9ff7dd057c8ad074ba9177d5e001d795
Wilgenfors <wilgenfors@mail.ru>
2025-01-24 13:44:07 +0500

    Update succefuly_log_in.php

--- succefuly_log_in.php
+++ succefuly_log_in.php
@@ -2,20 +2,19 @@
 session_start(); // Запускаем сессию, чтобы иметь возможность хранить и получать данные сессии
 
 // Проверяем, есть ли сообщение об успешной регистрации
-if (isset($_SESSION['success_message'])) {
-    // Если сообщение существует, выводим его
-    //echo htmlspecialchars($_SESSION['success_message']); // Выводим сообщение, экранируя специальные символы для безопасности
-    //unset($_SESSION['success_message']); // Очищаем сообщение после вывода, чтобы оно не отображалось повторно
-}
+$successMessage = isset($_SESSION['success_message']) ? $_SESSION['success_message'] : '';
+
+// Очищаем сообщение после его использования
+unset($_SESSION['success_message']);
 ?>
 
 <html lang="ru">
 <head>
     <meta charset="utf-8"> <!-- Устанавливаем кодировку страницы -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Настройки для адаптивного дизайна -->
-    <title>Succefuly sing up</title> <!-- Заголовок страницы -->
-    <meta property="og:title" content="Zagolovok stranitsy v OG"> <!-- Заголовок для Open Graph -->
-    <meta property="og:description" content="Opisanie stranitsy v OG"> <!-- Описание для Open Graph -->
+    <title>Успешная регистрация</title> <!-- Заголовок страницы -->
+    <meta property="og:title" content="Успешная регистрация на сайте"> <!-- Заголовок для Open Graph -->
+    <meta property="og:description" content="Данные успешно зарегистрированы."> <!-- Описание для Open Graph -->
     <meta property="og:image" content="https://example.com/image.jpg"> <!-- Изображение для Open Graph -->
     <meta property="og:url" content="https://example.com/"> <!-- URL для Open Graph -->
     <link rel="stylesheet" href="styles.css"> <!-- Подключаем CSS файл -->
@@ -24,6 +23,7 @@ if (isset($_SESSION['success_message'])) {
     <main>
         <form class="form"> <!-- Форма с классом "form" для применения стилей -->
             <h2>Регистрация прошла успешно</h2><br /> <!-- Заголовок формы -->
+            
         </form>
     </main>
 </body>
